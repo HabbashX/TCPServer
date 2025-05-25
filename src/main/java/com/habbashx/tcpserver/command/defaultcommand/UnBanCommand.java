f93a0dd7ed189e26434eec8766f65b27e0aefd07
HabbashX <abdalahalhabbash@gmail.com>
2025-05-25 12:39:13 +0300

    Update annotation from @PossibleEmpty to @MayBeEmpty
    Replaced the @PossibleEmpty annotation with @MayBeEmpty in UnBanCommand and UnMuteCommand classes to reflect updated naming conventions. This ensures consistency with the new annotation standard across the codebase.

--- src/main/java/com/habbashx/tcpserver/command/defaultcommand/UnBanCommand.java
+++ src/main/java/com/habbashx/tcpserver/command/defaultcommand/UnBanCommand.java
@@ -1,6 +1,6 @@
 package com.habbashx.tcpserver.command.defaultcommand;
 
-import com.habbashx.tcpserver.annotation.PossibleEmpty;
+import com.habbashx.tcpserver.annotation.MayBeEmpty;
 import com.habbashx.tcpserver.command.Command;
 import com.habbashx.tcpserver.command.CommandContext;
 import com.habbashx.tcpserver.command.CommandExecutor;
@@ -88,7 +88,7 @@ public final class UnBanCommand extends CommandExecutor {
             return;
         }
 
-        @PossibleEmpty
+        @MayBeEmpty
         String targetUsername = commandContext.getArgs().get(0);
         @Nullable
         UserDetails targetUser = server.getServerDataManager().getUserByUsername(targetUsername);
--- src/main/java/com/habbashx/tcpserver/command/defaultcommand/UnMuteCommand.java
+++ src/main/java/com/habbashx/tcpserver/command/defaultcommand/UnMuteCommand.java
@@ -1,6 +1,6 @@
 package com.habbashx.tcpserver.command.defaultcommand;
 
-import com.habbashx.tcpserver.annotation.PossibleEmpty;
+import com.habbashx.tcpserver.annotation.MayBeEmpty;
 import com.habbashx.tcpserver.command.Command;
 import com.habbashx.tcpserver.command.CommandContext;
 import com.habbashx.tcpserver.command.CommandExecutor;
@@ -74,7 +74,7 @@ public final class UnMuteCommand extends CommandExecutor {
             return;
         }
 
-        @PossibleEmpty
+        @MayBeEmpty
         String username = commandContext.getArgs().get(0);
         @Nullable
         UserHandler targetUser = server.getServerDataManager().getOnlineUserByUsername(username);
