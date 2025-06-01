f5f4910c742713762919470d22f190c8299afb3a
HabbashX <abdalahalhabbash@gmail.com>
2025-06-01 15:16:39 +0300

    Add `Container` annotation for metadata definition
    Introduce the `Container` annotation to define persistent file storage metadata and optional description for annotated classes. This aids in associating classes with non-volatile data files and provides contextual information for their usage.Add ExceptionUtils utility and Container annotation
    
    Introduce `ExceptionUtils` to convert exception stack traces to strings and a `Container` annotation for metadata on persistent file storage. This improves exception handling and annotation-based data management capabilities.

---
+++ src/main/java/com/habbashx/tcpserver/util/ExceptionUtils.java
@@ -0,0 +1,28 @@
+package com.habbashx.tcpserver.util;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A utility class that provides helper methods related to exception handling.
+ */
+public class ExceptionUtils {
+
+    /**
+     * Converts the stack trace of the specified exception into a string representation.
+     *
+     * @param e The exception from which the stack trace will be extracted. Must not be null.
+     * @return A string representation of the stack trace of the given exception.
+     */
+    public static @NotNull String getStackTrace(@NotNull Exception e) {
+
+        StackTraceElement[] stackTrace = e.getStackTrace();
+
+        StringBuilder builder = new StringBuilder();
+
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            builder.append(stackTraceElement.toString()).append("\n");
+        }
+
+        return builder.toString();
+    }
+}
