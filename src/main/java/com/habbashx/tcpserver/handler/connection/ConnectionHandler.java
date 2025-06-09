fba57b2d5dfb092ea1cdf9e3d9983898fd48a082
HabbashX <abdalahalhabbash@gmail.com>
2025-06-09 22:44:45 +0300

    Add dynamic connection settings injection to ConnectionHandler
    Implemented support for injecting connection settings dynamically using properties prefixed with "connection". Added methods

--- src/main/java/com/habbashx/tcpserver/handler/connection/ConnectionHandler.java
+++ src/main/java/com/habbashx/tcpserver/handler/connection/ConnectionHandler.java
@@ -1,19 +1,58 @@
 package com.habbashx.tcpserver.handler.connection;
 
+import com.habbashx.annotation.InjectPrefix;
+import com.habbashx.injector.PropertyInjector;
+import com.habbashx.tcpserver.handler.connection.configuration.ConnectionSettings;
+import com.habbashx.tcpserver.handler.connection.util.ConnectionUtils;
 import com.habbashx.tcpserver.security.Permissible;
-import com.habbashx.tcpserver.socket.Server;
+import com.habbashx.tcpserver.socket.server.Server;
 
 import javax.net.ssl.SSLSocket;
+import java.io.File;
+import java.net.SocketException;
 
 
+/**
+ * The ConnectionHandler abstract class is designed for managing secure
+ * connections using SSLSocket. It serves as a base class for specialized
+ * connection handlers that implement specific roles or functionalities.
+ * This class also applies certain connection settings and ensures that
+ * necessary configurations are injected.
+ * <p>
+ * ConnectionHandler implements the Permissible and Runnable interfaces,
+ * allowing it to handle permission-based logic and be executed as a
+ * concurrent task, respectively.
+ */
 public abstract class ConnectionHandler implements Permissible, Runnable {
 
     private final Server server;
     private final SSLSocket socket;
 
+    /**
+     * Configuration object that encapsulates the settings for establishing and managing
+     * connections within the {@code ConnectionHandler} class. These settings include
+     * properties such as buffering options, socket timeouts, and other network-related
+     * configurations that dictate the behavior of the connection.
+     * <p>
+     * The fields of this object are injected dynamically at runtime based on the
+     * specified prefix, allowing the settings to be customized and modified
+     * without altering the codebase. The {@code @InjectPrefix("connection")}
+     * annotation indicates that properties prefixed with "connection" in the
+     * configuration source will be injected into this instance.
+     * <p>
+     * The injected settings control factors such as:
+     * - Enabling or disabling socket timeout.
+     * - Configuring buffer size for data transmission.
+     * - Reuse of socket addresses.
+     * - Persistent or transient socket connections.
+     */
+    @InjectPrefix("connection")
+    private final ConnectionSettings connectionSettings = new ConnectionSettings();
+
     public ConnectionHandler(SSLSocket sslSocket, Server server) {
         this.server = server;
         this.socket = sslSocket;
+        injectConnectionSettings();
     }
 
     /**
@@ -39,5 +78,64 @@ public abstract class ConnectionHandler implements Permissible, Runnable {
 
     public SSLSocket getUserSocket() {
         return socket;
+    }
+
+    /**
+     * Configures the connection settings file and applies the respective properties for the current connection handler.
+     * <p>
+     * This method handles the following actions:
+     * - Generates or ensures the existence of a configuration file specific to the current connection handler by invoking
+     * {@code ConnectionUtils.generateConnectionHandlerFile}.
+     * - Retrieves the file path of the generated configuration file using {@code ConnectionUtils.getConnectionHandlerFile}.
+     * - Instantiates a {@code PropertyInjector} with the configuration file to dynamically inject the
+     * connection-related settings into the current connection handler instance.
+     * <p>
+     * The main purpose of this method is to ensure the ConnectionHandler instance is initialized with the necessary
+     * configurations derived from an external properties file, enabling customized connection behavior.
+     */
+    private void injectConnectionSettings() {
+        ConnectionUtils.generateConnectionHandlerFile(this, server.getServerLogger());
+
+        final String connectionHandlerFileName = ConnectionUtils.getConnectionHandlerFile(this);
+        final File file = new File(connectionHandlerFileName);
+        final PropertyInjector propertyInjector = new PropertyInjector(file);
+        propertyInjector.inject(this, socket, server);
+    }
+
+    /**
+     * Configures the settings for the underlying SSL socket based on the specified connection settings.
+     * This method ensures that appropriate socket options are applied, such as buffer sizes,
+     * timeouts, address reuse, and keep-alive settings, if the socket is currently connected.
+     * <p>
+     * The configuration relies on properties provided by the {@code connectionSettings} object:
+     * <ul>
+     * - If buffering is enabled, both receive and send buffer sizes will be set.
+     * - If socket timeout is enabled, the timeout value will be applied.
+     * - Address reuse and keep-alive settings will be applied directly based on their respective flags.
+     * </ul>
+     * <p>
+     * In case of a {@link SocketException}, the exception is logged using the server's logger.
+     * The method itself does not propagate the exception, ensuring safe execution without interruption.
+     * <p>
+     * It is recommended to invoke this method after establishing the SSL socket connection to apply the settings properly.
+     */
+    public void setupSettings() {
+
+        try {
+            if (socket.isConnected()) {
+
+                if (connectionSettings.isBufferingEnabled()) {
+                    socket.setReceiveBufferSize(connectionSettings.getBufferSize());
+                    socket.setSendBufferSize(connectionSettings.getBufferSize());
+                }
+                if (connectionSettings.isSoTimeOutEnabled()) {
+                    socket.setSoTimeout(connectionSettings.getSoTimeout());
+                }
+                socket.setReuseAddress(connectionSettings.isReuseAddress());
+                socket.setKeepAlive(connectionSettings.isKeepAlive());
+            }
+        } catch (SocketException e) {
+            server.getServerLogger().error(e);
+        }
     }
 }
