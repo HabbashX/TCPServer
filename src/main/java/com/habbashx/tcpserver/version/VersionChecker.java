1bd6299c2569f3328a8668d53a3bd1813ba88c2e
HabbashX <abdalahalhabbash@gmail.com>
2025-06-27 21:19:36 +0300

    Bump version to 1.1.3

--- src/main/java/com/habbashx/tcpserver/version/VersionChecker.java
+++ src/main/java/com/habbashx/tcpserver/version/VersionChecker.java
@@ -1,6 +1,6 @@
 package com.habbashx.tcpserver.version;
 
-import com.habbashx.tcpserver.socket.server.Server;
+import com.habbashx.tcpserver.socket.server.foundation.ServerFoundation;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 /**
  * The VersionChecker class is responsible for verifying if the current version
  * of the project matches the latest version available online.
@@ -19,9 +20,9 @@ public class VersionChecker {
      * Logs appropriate messages indicating whether the project is up-to-date or outdated.
      * In case of an error, logs an appropriate error message.
      *
-     * @param server An instance of the Server class that provides logger functionality.
+     * @param serverFoundation An instance of the ServerFoundation class that provides logger functionality.
      */
-    public static void checkProjectVersion(Server server) {
+    public static void checkProjectVersion(ServerFoundation serverFoundation) {
 
         try {
             final var url = new URL("https://raw.githubusercontent.com/HabbashX/TCPServer/main/src/main/java/com/habbashx/tcpserver/version/version.txt");
@@ -31,15 +32,15 @@ public class VersionChecker {
             if (connection.getResponseCode() == 200) {
 
                 try (final var currentVersionReader = new BufferedReader(new FileReader("src/main/java/com/habbashx/tcpserver/version/version.txt"));
-                     final var newestVersionReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                     final var newestVersionReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))
                 ) {
                     String currentVersion = currentVersionReader.readLine();
                     String newestVersion = newestVersionReader.readLine().trim();
 
                     if (currentVersion.contentEquals(newestVersion)) {
-                        server.getServerLogger().info("project version: " + currentVersion);
+                        serverFoundation.getServerLogger().info("project version: " + currentVersion);
                     } else {
-                        server.getServerLogger().warning("project is running on old version " + currentVersion + " update it to the new version " + newestVersion);
+                        serverFoundation.getServerLogger().warning("project is running on old version " + currentVersion + " update it to the new version " + newestVersion);
                     }
                 }
             }
