f7665df605448017e70a72fbaec13f8868c776e0
HabbashX <abdalahalhabbash@gmail.com>
2026-02-22 14:13:27 +0200

    Fix user join handling and improve logging messages in DefaultUserJoinHandler

--- src/main/java/com/habbashx/tcpserver/listener/handler/DefaultUserJoinHandler.java
+++ src/main/java/com/habbashx/tcpserver/listener/handler/DefaultUserJoinHandler.java
@@ -1,8 +1,8 @@
 package com.habbashx.tcpserver.listener.handler;
 
 import com.habbashx.tcpserver.event.Priority;
-import com.habbashx.tcpserver.event.handler.EventHandler;
 import com.habbashx.tcpserver.event.UserJoinEvent;
+import com.habbashx.tcpserver.event.handler.EventHandler;
 import com.habbashx.tcpserver.listener.Listener;
 import com.habbashx.tcpserver.socket.server.Server;
 import org.jetbrains.annotations.NotNull;
@@ -14,7 +14,7 @@ import static com.habbashx.tcpserver.logger.ConsoleColor.RESET;
  * The DefaultUserJoinHandler class is an implementation of the Listener interface
  * that handles `UserJoinEvent`. It is responsible for processing user join events
  * on the server.
- *
+ * <p>
  * This class checks if a user attempting to join the server is banned. If the user
  * is not banned, a join message is broadcasted to the chat, and the connection is
  * logged. If the user is banned, the join event is canceled, the user is notified,
@@ -33,15 +33,15 @@ public final class DefaultUserJoinHandler implements Listener<UserJoinEvent> {
     public void onEvent(@NotNull UserJoinEvent userJoinEvent) {
 
         if (!server.getBanCommandManager().isUserBanned(userJoinEvent.getUsername())) {
-            server.broadcast(userJoinEvent.getUsername() +" join the chat");
-            server.getServerLogger().info(userJoinEvent.getUsername() + " connected to server");
+            server.broadcast(userJoinEvent.getUsername() + " join the chat");
+            server.getAuthenticatedUsers().put(userJoinEvent.getUsername(), userJoinEvent.getUser());
             return;
         }
 
         userJoinEvent.setCancelled(true);
-        userJoinEvent.getUser().sendMessage(RED+"you are banned from server"+RESET);
+        userJoinEvent.getUser().sendMessage(RED + "you are banned from the server" + RESET);
         userJoinEvent.getUser().getUserDetails().setUsername(null);
-        server.getServerLogger().monitor("the user: "+userJoinEvent.getUsername() + " trying to join to server");
+        server.getServerLogger().monitor("the user: " + userJoinEvent.getUsername() + " trying to join to the server");
         userJoinEvent.getUser().shutdown();
 
     }
